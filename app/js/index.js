d1715e5f46f9a0d259fc70a4f18ff75bf672d876
Benjamin Snoha <benjamin@snoha.com>
2016-04-02 05:03:05 -0500

    Fixed styling and some database information for tracks

--- app/js/index.js
+++ app/js/index.js
@@ -1,31 +1,84 @@
 $(document).ready(function(){
-	getPosts();
-		
+	getTracks();
+	
+	//Set up soundcloud secret
 	SC.initialize({
 	  client_id: '58479d90aaeccef837849be331f895ca'
 	});
 
-	var track_url = 'http://soundcloud.com/forss/flickermood';
-	SC.oEmbed(track_url, { auto_play: false }).then(function(oEmbed) {
-	  console.log('oEmbed response: ', oEmbed);
-	  $('#trackContainer').append(oEmbed.html);
+	$(document).on('click', '.track', function(){		
+		var scContainer = $(this).find(".soundcloudContainer");
+	
+		if(scContainer.css("display") === "none"){
+			scContainer.html(
+				getEmbeddedPlayer(this.id, scContainer)
+			);
+		
+			scContainer.show();
+		}
+		else{
+			scContainer.hide();
+			scContainer.empty();
+		}
 	});
 	
+	//Admin only
+	//setTrack({name: "testName", genre: "Rap"});
 });
 
-function getPosts(){
+//Loads info about track
+function getTracks(){
 	$.ajax({
-		url: "/getPosts"
+		url: "/getTracks"
 	}).done(function(data) {
 		for(var i = 0; i < data.length; i++){
 			$('#trackContainer').append(
 				"<ul>" +
-					"<li class='track'>" +
-						data[i] +
+					"<li>" + 
+						"<div class='track' id=" + data[i].id + ">" +
+							"<div class='trackInfo'>" + data[i] + "</div>" + 
+							"<div class='soundcloudContainer'></div>" + 
+						"</div>" +
 					"</li>" +
 				"</ul>"
 			);
 		}
+	}).fail(function(xhr, status, error){
+		console.log("Status: " + status + " Error: " + error);
+		console.log(xhr);
+	});
+}
+
+/*
+	Format of trackToSet:
+	{name: x, artist: x, length: x, genre: x, url: x, rating: x}
+*/
+function setTrack(trackToSet){
+	$.ajax({
+		url: "/setTrack",
+		data: {
+			trackData: trackToSet
+		}
+	}).done(function(data) {
+		//Track has been added to database
+	}).fail(function(xhr, status, error){
+		console.log("Status: " + status + " Error: " + error);
+		console.log(xhr);
+	});
+}
+
+//Loads embedded track (media)
+function getEmbeddedPlayer(id, divID){
+	$.ajax({
+		url: "/getTrackByID",
+		data: {
+			trackID: id
+		}
+	}).done(function(data) {
+		//All info about track returned in data
+		SC.oEmbed(data.url, { auto_play: false }).then(function(oEmbed) {
+			$(divID).html(oEmbed.html);
+		});
 	}).fail(function(xhr, status, error){
 		console.log("Status: " + status + " Error: " + error);
 		console.log(xhr);
--- server.js
+++ server.js
@@ -20,8 +20,32 @@ pool.getConnection(function(error, connection){
 	});
 	
 	//Exchange information through ajax
-	app.get('/getPosts', function (req, res) {	
-		connection.query("SELECT * FROM `users`", function(errorQ1, rowsQ1){	
+	app.get('/getTracks', function (req, res) {	
+		connection.query("SELECT * FROM `tracks`", function(errorQ1, rowsQ1){	
+			if (errorQ1){
+				console.log("ERROR: " + errorQ1 + " : " + rowsQ1); return errorQ1;
+			}
+			else{					 
+				res.send(rowsQ1);
+			}
+		});
+	});
+	
+	//Exchange information through ajax
+	app.get('/getTrackByID', function (req, res) {	
+		connection.query("SELECT * FROM `tracks` WHERE id = " + req.query.trackID, function(errorQ1, rowsQ1){	
+			if (errorQ1){
+				console.log("ERROR: " + errorQ1 + " : " + rowsQ1); return errorQ1;
+			}
+			else{					 
+				res.send(rowsQ1[0]);
+			}
+		});
+	});
+	
+	//Exchange information through ajax
+	app.get('/setTrack', function (req, res) {	
+		connection.query("INSERT INTO `tracks` SET ?", req.query.trackData, function(errorQ1, rowsQ1){	
 			if (errorQ1){
 				console.log("ERROR: " + errorQ1 + " : " + rowsQ1); return errorQ1;
 			}
@@ -32,6 +56,6 @@ pool.getConnection(function(error, connection){
 	});
 	
 	app.listen(3000, function () {
-		console.log('Example app listening on port 3000!');
+		console.log('Listening on port 3000');
 	});
 });
