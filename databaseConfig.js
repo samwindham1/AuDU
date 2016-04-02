c889a0be31ceda8fca4a57e48f958592df451425
samwindham1 <sam.windham95@gmail.com>
2016-04-02 03:14:29 -0600

    gitignore errors

--- databaseConfig.js
+++ databaseConfig.js
@@ -1,10 +1,9 @@
-// config/database.js
 module.exports = {
     'localConnection': {
         'host'		: 'localhost',
         'port'		: '3306',
         'user'		: 'root',
-        'password'  : '',
+        'password'  : 'Samlogin1995',
     	'charset'   : 'UTF8MB4_GENERAL_CI'
     },
 	'database': 'audu'
