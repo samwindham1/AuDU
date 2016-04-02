c30722de87602bc1315a43fd56efcedc675cf57f
samwindham1 <sam.windham95@gmail.com>
2016-04-02 00:40:43 -0600

    set up index.html

--- databaseConfig.js
+++ databaseConfig.js
@@ -4,7 +4,7 @@ module.exports = {
         'host'		: 'localhost',
         'port'		: '3306',
         'user'		: 'root',
-        'password'  : '',
+        'password'  : 'Samlogin1995',
     	'charset'   : 'UTF8MB4_GENERAL_CI'
     },
 	'database': 'audu'
