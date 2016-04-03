8ab5da829382753594bd3ad5b3790b6a0d87c275
samwindham1 <sam.windham95@gmail.com>
2016-04-03 04:05:20 -0600

    cleanin up shop

--- server.js
+++ server.js
@@ -4,37 +4,11 @@ var app = express();
 
 app.use(express.static(__dirname + '/app'));
 
-//Database requirements
-var mysql = require('mysql');
-var dbconfig = require('./databaseConfig');
-var pool = mysql.createPool(dbconfig.localConnection);
+//Basic page loading
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/app/views/index.html');
+});
 
-//Connect to the pool
-pool.getConnection(function(error, connection){	
-	//Select the database
-	connection.query('USE ' + dbconfig.database);
-	
-	//Basic page loading
-	app.get('/', function (req, res) {
-		res.sendFile(__dirname + '/app/views/index.html');
-	});
-	
-	//Exchange information through ajax
-	app.get('/getTracks', function (req, res) {	
-		
-		
-		
-		
-		function(result){
-			res.send(result);
-		}
-		
-		
-		
-		return;
-	});
-	
-	app.listen(3000, function () {
-		console.log('Listening on port 3000');
-	});
+app.listen(3000, function () {
+	console.log('Listening on port 3000');
 });
